44eea97512ca162f2f7d45e9850083d4ec4a08a5
Eashan Mudgal <eashanmudgalaan@gmail.com>
2020-10-01 14:59:25 +0530

    Modified alpha_blog

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,33 @@
 class UsersController < ApplicationController
 
-def new
+def show
+ @user= User.find(params[:id])
+ @articles=@user.articles
+end
 
-	@user = User.new
+def index
+  @users=User.all
 
 end
+
+def new
+  @user = User.new
+end
+
+def edit
+    @user=User.find(params[:id])
+end
+
+def update
+	 @user=User.find(params[:id])
+	 if @user.update(user_params)
+	 flash[:notice]= "Your account information was successfully updated"
+	 redirect_to @user
+     else
+		render 'edit'
+     end
+end
+
 
 def create
 	@user=User.new(user_params)
